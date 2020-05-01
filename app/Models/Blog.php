b60842f6affd17502ca75f75ca09d24d5a4802a6
RazaChohan <razachohan@gmail.com>
2020-05-01 15:10:52 +0200

    Show blog data on main page

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -24,6 +24,6 @@ class Blog extends BaseModel
      */
     public function getBlogs($page = 1, $limit = 3)
     {
-        return $this->getRecordsWithPage($this->tableName, null, $page, $limit);
+        return $this->getRecordsWithPage($this->tableName, "select b.title, b.content, b.created, a.first_name as author_first_name, a.last_name as author_last_name from blogs b join authors a on a.id = b.author_id", $page, $limit);
     }
 }
--- app/Views/main.php
+++ app/Views/main.php
@@ -1,4 +1,14 @@
-  <html>
-    <h1>Welcome to Blog Application</h1>
-    <a href="/login">Login Here..</a>
-  </html>
+<html>
+<h1>Welcome to Blog Application</h1>
+<a href="/login">Login Here..</a>
+<div>
+    <?php if (!empty($data['blogs'])): ?>
+        <?php foreach ($data['blogs'] as $blog): ?>
+            <div><?php echo $blog->title ?></div>
+            <div><?php echo $blog->content ?></div>
+            <div><?php echo $blog->author_first_name . ' ' . $blog->author_last_name ?></div>
+            <div><?php echo $blog->created ?></div>
+        <?php endforeach; ?>
+    <?php endif; ?>
+</div>
+</html>
